17576bd144da011d7fbc9e641792a1595f13d956
Guilherme Pontes <guilherme-pontes2015@hotmail.com>
2022-05-01 14:54:58 -0300

    projeto inicial

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import {useState} from 'react';
+function Home() {
+    return (
+        <div>
+            <h1>Contador</h1>
+          
+        </div>
+    )
+    
+}
+
+
+
+export default Home
+
